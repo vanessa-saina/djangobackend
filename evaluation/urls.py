1e1219678f3f177613e47f22eef85c2acfab028b
Elvin Ega <proega3@gmail.com>
2018-03-27 00:36:44 +0300

    Fix view questions

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
 	url(r'^create_evaluation/$', create_evaluation),
 	url(r'^create_question/$', create_question),
-	url(r'^view_question/$', view_question)
+	url(r'^view_questions/$', view_questions)
 ]
 
